519dbff5941c9551456ba342ea14dc4b533e19d8
AYMEN <moniakamli95@gmail.com>
2021-05-23 11:48:01 +0200

    initial commit

---
+++ src/styles.js
@@ -0,0 +1,24 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 0',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      heading: {
+        color: 'rgb(255,20,147)',
+      },
+      image: {
+        marginLeft: '15px',
+        borderRadius: '8px',
+      },
+      [theme.breakpoints.down('sm')]: {
+        mainContainer: {
+          flexDirection: "column-reverse",
+      },
+      }
+}));
